9184a2f0b2344e5c8c74c92c0f79c0550bf5414c
i-gabdrahmanov <alex.geo.kpp@icloud.com>
2024-06-12 11:34:54 +0300

    init

---
+++ src/main/java/org/homedev/prototypes/DictionarySearchPrototypeApplication.java
@@ -0,0 +1,13 @@
+package org.homedev.prototypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionarySearchPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionarySearchPrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/homedev/prototypes/DictionarySearchPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.homedev.prototypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionarySearchPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
