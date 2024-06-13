24f6b96847abe791bcdb63920684184f3779a1d2
i-gabdrahmanov <alex.geo.kpp@icloud.com>
2024-06-13 22:53:29 +0300

    add first digits search for inn

--- src/main/java/org/homedev/prototypes/repository/PersonRepository.java
+++ src/main/java/org/homedev/prototypes/repository/PersonRepository.java
@@ -3,6 +3,7 @@ package org.homedev.prototypes.repository;
 import org.homedev.prototypes.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +12,9 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     @Query(value = "select id from person order by id desc limit 1", nativeQuery = true)
     Optional<Long> findLastId();
 
-    List<Person> findAllByInnStartingWith(String prefix);
+    /*@Query(value = "select * from person where inn like '?1%' limit 50", nativeQuery = true)
+    List<Person> findAllByInnStartingWith(String prefix);*/
+
+    @Query(value = "SELECT * FROM person WHERE inn LIKE :prefix || '%' LIMIT 20", nativeQuery = true)
+    List<Person> findAllByInnStartingWith(@Param("prefix") String prefix);
 }
